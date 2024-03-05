362e8e69a93838f8c8e69381cba3aa7fc859a7d5
DivyatejaChe <divyateja.chepuru@bain.com>
2024-03-05 14:17:22 +0530

    Rendering multiple MFEs on same screen

--- apps/ncbApp/src/screens/AuthScreen.tsx
+++ apps/ncbApp/src/screens/AuthScreen.tsx
@@ -1,15 +1,20 @@
 import {Federated} from '@callstack/repack/client';
 import React from 'react';
-import { Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 
 const Auth = React.lazy(() => Federated.importModule('auth', './App'));
-
+const Transactions = React.lazy(() => Federated.importModule('transactions', './App'));
 
 const AuthScreen = () => {
   return (
+    <ScrollView>
     <React.Suspense fallback={<Text>Loading Auth...</Text>}>
         <Auth />
     </React.Suspense>
+    <React.Suspense fallback={<Text>Loading Auth...</Text>}>
+        <Transactions />
+    </React.Suspense>
+    </ScrollView>
   );
 };
 
--- backend/user-module-server/index.js
+++ backend/user-module-server/index.js
@@ -4,14 +4,13 @@ const app = express();
 const port = 3000;
 
 /**
- * auth: "https://cdn.jsdelivr.net/gh/DivyatejaChe/df-repack@1629c48afc875fe2afb72e251f5d24c6b9cd6c66/packages/catalog-server/data/[name][ext]",
- * auth: "https://cdn.jsdelivr.net/gh/DivyatejaChe/df-repack@58a34d19ac7d0a9947ebcf63c56c7564c13ae210/bundles/build/generated/android/[name][ext]",
  *
  * auth: "http://localhost:9000/[name][ext]",
+ * auth: "https://cdn.jsdelivr.net/gh/DivyatejaChe/df-repack@d996b438ed10c0f5cb3242990c383f55e9ac3900/bundles/build/generated/android/[name][ext]",
  */
 
 const userModuleAccess = {
-  auth: "https://cdn.jsdelivr.net/gh/DivyatejaChe/df-repack@d996b438ed10c0f5cb3242990c383f55e9ac3900/bundles/build/generated/android/[name][ext]",
+  auth: "http://localhost:9000/[name][ext]",
   transactions: "http://localhost:9001/[name][ext]",
   help: "http://localhost:9002/[name][ext]",
 };
